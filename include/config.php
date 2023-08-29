bbe55e5ad8601fa39389f3766f8d42b7a1bbae5b
gegerung <143520726+gegerung@users.noreply.github.com>
2023-08-29 21:40:47 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,6 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+
+$data['Gegerung-Network'] = array ('1'=>'Gegerung-Network!13.12.95.1','Gegerung-Network@|@roney','Gegerung-Network#|#qqCgnapja2JkamE=','Gegerung-Network%GEGERUNG-NET','Gegerung-Network^gegerung.net','Gegerung-Network&Rp','Gegerung-Network*10','Gegerung-Network(1','Gegerung-Network)','Gegerung-Network=disable','Gegerung-Network@!@enable');
